c2049a025a401a3c9df08f919745e25ff40d1ee9
oseongryu <oseongryu@gmail.com>
2024-11-06 17:04:21 +0900

    chore: routes item

--- app.py
+++ app.py
@@ -13,13 +13,32 @@ from flask import (
 
 from routes.routes_aws import routes_aws
 
-# sys.path.append(os.path.join(os.path.dirname(__file__), "aws"))
-# sys.path.append(os.path.join(os.path.dirname(__file__), "common"))
-
-
 app = Flask(__name__, template_folder="templates", static_url_path="/static", static_folder="static")
 
 load_dotenv()
+
+# Load and parse the array from the .env file
+routes_item = os.getenv("routes_item", "").split(",")
+
+if "shorts" in routes_item:
+    from routes.routes_automation import routes_automation
+    from routes.routes_common import routes_common
+    from routes.routes_shorts import routes_shorts
+
+    app.register_blueprint(routes_automation)
+    app.register_blueprint(routes_common)
+    app.register_blueprint(routes_shorts)
+
+    from flask_mysqldb import MySQL
+
+    app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+    app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
+    app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+    app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+    mysql = MySQL(app)
+    app.mysql = mysql
+
 app.register_blueprint(routes_aws)
 
 
