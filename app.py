bbf5d372cf5beb5b5ffa1da28815aacb2c142743
oseongryu <oseongryu@gmail.com>
2024-12-03 20:23:33 +0900

    fix: app.py

--- app.py
+++ app.py
@@ -35,34 +35,32 @@ app.AUTOMATION_SETTING = config.AUTOMATION_SETTING
 load_dotenv()
 
 # Load and parse the array from the .env file
-routes_item = os.getenv("routes_item", "").split(",")
+routes_items = os.getenv("routes_item", "").split(",")
 
+for routes_item in routes_items:
+    if "shorts" in routes_item:
+        from flask_mysqldb import MySQL
 
-if "shorts" in routes_item:
-    from flask_mysqldb import MySQL
+        from routes.routes_shorts import routes_shorts
 
-    from routes.routes_shorts import routes_shorts
+        app.register_blueprint(routes_shorts)
 
-    app.register_blueprint(routes_shorts)
+        app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+        app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
+        app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+        app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+        app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+        mysql = MySQL(app)
+        app.mysql = mysql
+    elif "aws" in routes_item:
+        from routes.routes_aws import routes_aws
 
-    app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
-    app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
-    app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
-    app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
-    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
-    mysql = MySQL(app)
-    app.mysql = mysql
+        app.register_blueprint(routes_aws)
 
+    elif "automation" in routes_item:
+        from routes.routes_automation import routes_automation
 
-if "aws" in routes_item:
-    from routes.routes_aws import routes_aws
-
-    app.register_blueprint(routes_aws)
-
-if "automation" in routes_item:
-    from routes.routes_automation import routes_automation
-
-    app.register_blueprint(routes_automation)
+        app.register_blueprint(routes_automation)
 
 
 if __name__ == "__main__":
