823c1e9239f7df9da99f570689cc38fb43c739e3
oseongryu <oseongryu@gmail.com>
2025-02-07 14:34:57 +0900

    feat: fredit

---
+++ service/fredit/log_time_sort.py
@@ -0,0 +1,60 @@
+import glob
+import os
+import re
+from datetime import datetime
+import posixpath
+
+# 현재 날짜와 시간을 가져옵니다.
+now = datetime.now()
+
+targets = ["RSL_MSG=기타 오류"]
+ex_targets = []
+
+output_log_path = posixpath.join(os.path.expanduser("~"), "fredit_log")
+output_file_name = now.strftime("%Y%m%d%H%M%S") + '.txt'
+log_paths = [posixpath.join(output_log_path, f"was{i}") for i in range(1, 5)]
+
+
+def check_all_targets_satisfied(line):
+    # 모든 조건이 True인지 확인
+    return all(condition in line for condition in targets)
+
+def check_any_ex_targets_satisfied(line):
+    return any(condition in line for condition in ex_targets)
+
+# 로그 파일들에서 로그 라인을 추출
+log_lines = []
+for log_path in log_paths:
+
+    # 로그 파일 경로 패턴
+    log_files_pattern = log_path + "/*"
+
+
+
+    # Regular expression to match the timestamp
+    timestamp_pattern = re.compile(r"^\d{2}:\d{2}:\d{2}\.\d{3}")
+
+    for log_file_path in sorted(glob.glob(log_files_pattern)):
+        if "txt" not in log_file_path:
+            with open(log_file_path, "r") as file:
+                print(log_file_path)
+                for line in file:
+
+                    match = timestamp_pattern.match(line)
+                    if match:
+                        date_obj = datetime.strptime(line[:12], "%H:%M:%S.%f")
+                        if check_any_ex_targets_satisfied(line) == False:
+                            if check_all_targets_satisfied(line):
+                                # line = line.split("fredit-token")[1].replace(": ", "")
+                                log_lines.append((date_obj, line))
+
+
+# 시간 순으로 로그 라인 정렬
+log_lines.sort(key=lambda x: x[0])
+print("sorted")
+# 분석 결과를 저장할 파일 경로
+output_file_path = posixpath.join(output_log_path, output_file_name)
+# 정렬된 줄을 파일에 쓰기
+with open(output_file_path, "w") as sorted_file:
+    for _, line in log_lines:
+        sorted_file.write(line)
---
+++ service/fredit/youtube.py
@@ -0,0 +1,98 @@
+import os
+import platform
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+from common.commonlogging import CommonLogging
+from dotenv import load_dotenv
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class YouTubeScraper:
+    def __init__(self, url):
+        platformName = platform.system()
+        platformArchitecher = platform.machine()
+        os.environ["WDM_LOG"] = "0"
+
+        if platformArchitecher == "aarch64":
+            os.environ["WDM_ARCH"] = "arm64"
+        elif platformArchitecher == "arm64":
+            os.environ["WDM_ARCH"] = "arm64"
+        else:
+            os.environ["WDM_ARCH"] = "x64"
+
+        self.url = url
+
+    def scrape(self):
+        selenium_use = True
+        html = None
+
+        if selenium_use:
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            options.add_experimental_option("detach", True)
+            options.add_argument("headless")
+            options.add_argument("no-sandbox")
+            options.add_argument("disable-dev-shm-usage")
+            options.add_argument("window-size=1920x1080")
+            options.add_argument("disable-gpu")
+            # https://kangmanjoo.tistory.com/122
+            options.add_argument("--log-level=3")
+            options.add_argument("--disable-loging")
+            options.add_argument(UserAgent().chrome)
+
+            serviceDriver = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=serviceDriver, options=options)
+
+            driver.get(self.url)
+            html = driver.page_source
+        else:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                html = response.text
+            else:
+                print("Failed to load the page. Status code:", response.status_code)
+
+        soup = BeautifulSoup(html, "html.parser")
+        # soup = BeautifulSoup(html)
+        # soup = BeautifulSoup(html, 'lxml')
+        # soup = BeautifulSoup(html, 'html5lib')
+        # soup = BeautifulSoup(html)
+        cards = soup.find_all("ytd-vertical-product-card-renderer")
+
+        text = ""
+        cnt = 0
+        for index, card in enumerate(cards):
+            # aria_label = card.find('a')['aria-label']
+            # print(aria_label)
+            product_name_element = card.find("div", {"id": "product-name"})
+            price_element = card.find("span", {"id": "price"})
+            if product_name_element:
+                # print(product_name_div.text)
+                if index == len(cards) - 1:  # 마지막 카드인 경우
+                    text += product_name_element.text + "[" + price_element.text.replace(",", "").replace("₩", "") + "]"
+                    cnt += 1
+                else:
+                    text += product_name_element.text + "[" + price_element.text.replace(",", "").replace("₩", "") + "]" + ","
+                    cnt += 1
+        print(cnt, text)
+        logger.info(str(cnt) + " " + text)
+        if selenium_use:
+            driver.quit()
+            # driver.close()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    youtube_url = os.getenv("YOUTUBE_CRAWLING_URL")
+    logFileName = os.path.expanduser("~") + "/youtube.log"
+    logger = CommonLogging(logFileName).logger
+    scraper = YouTubeScraper(youtube_url)
+    scraper.scrape()
